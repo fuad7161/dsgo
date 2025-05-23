58aa1796765d5b527fc7740aab6bde1a0e649590
fuadul <fuadul202@gmail.com>
2025-05-23 16:40:22 +0600

    ff

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"fmt"
-	dsgo "main/LinearDataStructures"
-)
+import "fmt"
 
 func main() {
-	var q = dsgo.NewQueue[int]()
-	q.Push(100)
-	val, _ := q.Pop()
-	fmt.Println(val)
+
+	fmt.Println("Hello World")
+
 }
